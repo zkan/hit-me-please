53d2b4e4624518b8d61317dc8de1c241baa47b40
zkan <kan@prontomarketing.com>
2019-06-01 14:23:21 +0700

    :sparkles: Render landing page view with form :bear:

--- hit_me_please/hitters/views.py
+++ hit_me_please/hitters/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        html = '<form action="." method="post">'
+        html += '<input type="email" name="email">'
+        html += '<button type="submit">Submit</button>'
+        html += '</form>'
+        return HttpResponse(html)
+
