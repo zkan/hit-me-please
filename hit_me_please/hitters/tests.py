9f286edf9713521d07c754ba7910bb9343766c84
zkan <kan@prontomarketing.com>
2019-06-01 11:39:18 +0700

    :sparkles: Register Hitter to admin :bear:

--- hit_me_please/hitters/tests.py
+++ hit_me_please/hitters/tests.py
@@ -1,5 +1,7 @@
+from django.contrib import admin
 from django.test import TestCase
 
+from .admin import HitterAdmin
 from .models import Hitter
 
 
@@ -9,3 +11,14 @@ class HitterTest(TestCase):
             email='kan@prontomarketing.com'
         )
         self.assertEqual(hitter.email, 'kan@prontomarketing.com')
+
+
+class HitterAdminTest(TestCase):
+    def test_hitter_should_be_registed_to_admin(self):
+        self.assertIsInstance(
+            admin.site._registry[Hitter],
+            HitterAdmin
+        )
+
+
+
