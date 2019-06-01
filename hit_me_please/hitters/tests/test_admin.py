91d7d0acf4305a0bca667136e33520897f08b2c0
zkan <kan@prontomarketing.com>
2019-06-01 13:34:44 +0700

    :white_check_mark: Reorganize tests :bear:

--- hit_me_please/hitters/tests/test_admin.py
+++ hit_me_please/hitters/tests/test_admin.py
@@ -1,16 +1,8 @@
 from django.contrib import admin
 from django.test import TestCase
 
-from .admin import HitterAdmin
-from .models import Hitter
-
-
-class HitterTest(TestCase):
-    def test_hitter_model_should_have_email_field(self):
-        hitter = Hitter.objects.create(
-            email='kan@prontomarketing.com'
-        )
-        self.assertEqual(hitter.email, 'kan@prontomarketing.com')
+from ..admin import HitterAdmin
+from ..models import Hitter
 
 
 class HitterAdminTest(TestCase):
