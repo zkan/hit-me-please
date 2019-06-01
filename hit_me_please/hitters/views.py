f14e69360045e95d56753e14e76cb22f2b31faff
zkan <kan@prontomarketing.com>
2019-06-01 14:30:37 +0700

    :recycle: Refactor HTML to template :bear:

--- hit_me_please/hitters/views.py
+++ hit_me_please/hitters/views.py
@@ -1,12 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
 
 
 class LandingPageView(View):
     def get(self, request):
-        html = '<form action="." method="post">'
-        html += '<input type="email" name="email">'
-        html += '<button type="submit">Submit</button>'
-        html += '</form>'
-        return HttpResponse(html)
+        return render(
+            request,
+            'index.html'
+        )
 
