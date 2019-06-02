ecd6d4c4f7dbd7d15d261ac1bcc9c06c3e0b1a2b
zkan <kan@prontomarketing.com>
2019-06-02 10:22:44 +0700

    :wrench: Create one simple compute instance using Terraform :bear:

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,34 @@
 provider "google" {
   credentials = "${file("hitmebaby.json")}"
   project     = "prontotools-212000"
-  region      = "asia-southeast1"
-  zone        = "asia-southeast1-b"
+  region      = "asia-south1"
+  zone        = "asia-south1-c"
 }
 
+resource "google_compute_instance" "tf_instance" {
+  name         = "tf-instance-${count.index}"
+  machine_type = "n1-standard-2"
+  count        = 1
 
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20190530"
+      size  = 30
+    }
+  }
+
+  metadata = {
+    sshKeys = "kan:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+
+  tags = ["my-web"]
+}
+
+output "ip" {
+  value = "${google_compute_instance.tf_instance.*.network_interface.0.access_config.0.nat_ip}"
+}
